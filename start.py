933016e313ba0c740114e4de2a3a2dc38d3f204f
progover <egor.shcherbak.00@mail.ru>
2018-02-05 13:46:12 +0300

    Add system info output

--- start.py
+++ start.py
@@ -1,4 +1,7 @@
-
+import platform
 print('PythonTestGitProject')
-
-
+print(platform.system())
+print(platform.platform())
+print(platform.machine())
+print(platform.version())
+print(platform.processor())
